bb7ca5f59c055f42aaf3de6d4447b30cad81a63c
Arun1krish <psharmila56@gmail.com>
2022-12-30 11:22:03 +0530

    firstprogramme

---
+++ src/org/arraydemo/DemoArray.java
@@ -0,0 +1,70 @@
+package org.arraydemo;
+
+public class DemoArray {
+	
+	public static void main(String[] args) {
+		
+
+	
+	int []a= new int []{1,2,3,3,4,4,6,7,8,1}; 
+	
+
+	
+	for (int i = 0; i < 10; i++) {
+		
+	for (int j = i+1; j < 10; j++) {
+		if(a[j]==a[i]) {
+		
+			System.out.println(a[j]);
+			
+		
+		}
+		
+		
+	}
+	
+        
+		}
+		
+	}
+	
+			
+				
+				
+				
+	}	
+			
+			
+		
+		
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
